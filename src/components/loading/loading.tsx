4d554037889f5f7f1d2a479740678fe9fda087a1
Pitsanu Limpanachaipornkul <pitsanu.li@kskgroup.com>
2021-02-08 23:59:56 +0700

    detail

--- src/components/loading/loading.tsx
+++ src/components/loading/loading.tsx
@@ -1,4 +1,6 @@
-export const LoadingWidget = () => (
+import React from "react";
+
+export const LoadingWidget: React.FC = () => (
   <div className="relative flex justify-center items-center h-screen">
     <div className="inline-block animate-spin ease duration-300 w-5 h-5 bg-black mx-2" />
     <div className="inline-block animate-ping ease duration-300 w-5 h-5 bg-black mx-2" />
