27726e12d89e010749eff24f8ef15ffefc0af34a
Pitsanu Limpanachaipornkul <pitsanu.li@kskgroup.com>
2021-02-09 14:59:02 +0700

    update tags

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -36,7 +36,7 @@ const Index = () => {
       <div className="max-w-4xl flex items-center h-auto lg:h-screen flex-wrap mx-auto py-32 lg:my-0 z-40">
         <div
           id="profile"
-          className="w-full lg:w-3/5 rounded-lg lg:rounded-l-lg lg:rounded-r-none shadow-2xl bg-white opacity-75 mx-6 lg:mx-0"
+          className="w-full lg:w-3/5 rounded-lg lg:rounded-l-lg lg:rounded-r-lg shadow-2xl bg-white opacity-75 mx-2 lg:mx-0"
         >
           <div className="p-4 md:p-12 text-center lg:text-left">
             <div
